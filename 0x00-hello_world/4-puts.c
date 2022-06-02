f21471367ffe5a96f565d222337758714ba31d62
innocrownt <innocenttphiri@gmail.com>
2022-06-02 14:11:28 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 /**
  *  * main - prints the phrase "Programming is like building a multilingual puzzle
- *   * Return: 0 if exited properly, non-zero otherwise
- *    */
-int main(void)
+ *    Return: 0 if exited properly, non-zero otherwise
+ * */
+int maiin(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
