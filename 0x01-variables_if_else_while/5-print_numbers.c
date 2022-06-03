56ca539bd23c66a4e5716c11d76da140c9e8d28d
innocrownt <innocenttphiri@gmail.com>
2022-06-03 12:03:25 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  * main - prints all single digit numbers of base
+ *   * 10 starting from 0, followed by a new line
+ *     */
+int main(void)
+{
+
+		for (int no = 0; no <= 9; no++){
+
+		printf("%d", no);
+		printf("\n");	
+			}
+		return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  * main - main function
+ *   *
+ *    * Return: end of the program
+ *     */
+int main(void)
+{
+
+		for (int n = '0'; n <= '9'; ++n)
+		putchar(n);
+
+		putchar(10);
+
+		return (0);
+}
