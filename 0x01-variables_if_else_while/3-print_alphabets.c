bc4866eb320372c6ee585930a80712ad610f4b0c
innocrownt <innocenttphiri@gmail.com>
2022-06-03 06:57:22 -0700

    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  * main - main function
+ *   *
+ *    * Return: end program
+ *     */
+int main(void)
+{
+	char cha;
+		char CHA;
+
+			for (cha = 'a'; cha <= 'z'; ++cha)
+			putchar("%c", cha);
+
+			for (CHA = 'A'; CHA <= 'Z'; ++CHA)
+			putchar("%c", CHA);
+
+		putchar("\n");
+
+		return (0);
+}
