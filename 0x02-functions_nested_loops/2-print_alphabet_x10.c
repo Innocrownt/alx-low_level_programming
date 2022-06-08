18c20b17f730dceddec1d2ea8d02680e16bfc38e
innocrownt <innocenttphiri@gmail.com>
2022-06-08 02:32:38 -0700

    alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,22 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *  * print_alphabet_x10 - Prints the alphabet 10 times.
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *print_alphabet_x10 - prints a-z 10 times.
+ *
+ *Return: 0
+ */
+
 void print_alphabet_x10(void)
 {
 	int a, b;
 
 	for (a = 0; a <= 9; a++)
-			{
+		{
 	for (b = 97; b <= 122; b++)
 		{
 	_putchar(b);
-			}
+																			}		
 	_putchar('\n');
-			}
+
+		}
 }
