4470da3d5a4fa0166a6a1d46d5419173dd642781
innocrownt <innocenttphiri@gmail.com>
2022-06-08 02:17:29 -0700

    all

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *  * print_alphabet - Prints the alphabet in lowercase.
+ *  * print_alphabet - prints alphabets in lowercase
  *   *
- *    * Return: Always 0 (Success)
+ *    * Return: 0
  *     */
+
 void print_alphabet(void)
 {
-	int i;
+		char alpha;
 
-		for (i = 97; i <= 122; i++)
-			{
-		_putchar(i);
-			}
-		_putchar('\n');
+			alpha = 'a';
+
+				while (alpha <= 'z')
+						{
+									_putchar(alpha);
+											alpha++;
+												}
+					_putchar('\n');
 }
